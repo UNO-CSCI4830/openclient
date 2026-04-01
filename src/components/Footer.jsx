7544bcb53b0e85c45eb771fc73e5785b81f8234a
hunterpope03 <hunter.pope23@gmail.com>
2026-04-01 14:14:24 -0500

    refactor: start UI redesign

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="app-footer">
-      <p>openclient &copy; {new Date().getFullYear()}</p>
+      <p>OpenClient &copy; {new Date().getFullYear()}</p>
     </footer>
   )
 }
